bfcf4df977142481baeabe94e3c8515ed1772c78
saulhervas <saulhervasgallego@gmail.com>
2024-05-14 19:42:29 +0200

    search apiService

--- app/src/main/java/com/saulhervas/laligateamsapp/utils/ApiService.kt
+++ app/src/main/java/com/saulhervas/laligateamsapp/utils/ApiService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("/teams")
-    suspend fun getTeams(@Query("name") teamName: String): Response<TeamsDataResponse>
+    suspend fun getTeams(@Query("search") teamName: String): Response<TeamsDataResponse>
 
     @GET("/teams")
     suspend fun getTeam(@Query("id") teamId: Int): Response<TeamsDataResponse>
